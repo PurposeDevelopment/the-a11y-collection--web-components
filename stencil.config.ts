1491627e0b97d3fba9629274f1144c05fc53aa03
Karl Baumhauer <48211526+karlbaumhauer@users.noreply.github.com>
2022-04-17 13:01:29 +0200

    refactor: renaming stuff

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@stencil/core';
 
 export const config: Config = {
-  namespace: 'stencil-storybook-html',
+  namespace: 'the-a11y-collection--web-components',
   outputTargets: [
     {
       type: 'dist',
