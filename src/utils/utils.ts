4fea4eaa35f067c7a0f88fb84eebdf4239b504f7
Karl Baumhauer <48211526+karlbaumhauer@users.noreply.github.com>
2022-04-18 16:42:56 +0200

    feat(the-a11y-icon): add new the-a11y-icon component

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,14 @@
 export function format(first: string, middle: string, last: string): string {
   return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
 }
+
+export async function fetchSvg(iconName: string): Promise<string> {
+  try {
+    return await fetch(`https://api.multiavatar.com/${iconName}.svg`)
+      .then((res: any) => (res.ok ? res.text() : '')).then((text) => {
+        return text.replace(/width=".*?"|height=".*?"/g, '');
+      });
+  } catch {
+    console.log(`Not able to get an svg for "${iconName}".`);
+  }
+}
