56ff91137513022fa04fb32de5fe0324ce876e9d
Karl Baumhauer <48211526+karlbaumhauer@users.noreply.github.com>
2022-04-19 21:56:50 +0200

    docs(the-a11y-icon): add story file for the-a11y-icon component

---
+++ src/components/the-a11y-icon/the-a11y-icon.stories.ts
@@ -0,0 +1,6 @@
+export default {
+    // this creates a ‘Components’ folder and a ‘TheA11yIcon’ subfolder
+    title: 'Components/TheA11yIcon',
+};
+
+export const configurable = () => `<the-a11y-icon icon="A11y"></the-a11y-icon>`;
